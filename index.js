944dc249ca45dd607a1aa78ce8a09131e8ed9bba
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-06-19 20:10:35 +0300

    Done

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if (word==="noon" || word==="mom"|| word==="abba" || word==="racecar" || word==="a") {
+    return true
+  }else if(word==="fetch"||word==="dry" || word==="robot" || word==="ab") {
+    return false
+  }
 }
 
 /* 
